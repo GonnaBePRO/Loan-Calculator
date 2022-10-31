e756e8a23811bd7b519e18c771642930ace2b221
GonnaBePRO <michu9139@gmail.com>
2022-10-31 17:05:41 +0000

    Loader & User Experience
    Loader & User Experience

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    // Hide results
+    document.getElementById('results').style.display =  'none';
+    
+    // Show loader
+    document.getElementById('loading').style.display =  'block';
+
+    setTimeout(calculateResults, 1000);
+
+    e.preventDefault();
+});
 
 // Calculate Results
 function calculateResults(e) {
@@ -24,15 +34,24 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayment).toFixed(2);
         totalInterest.value = ((monthly*calculatedPayment) - principal).toFixed(2); 
+        // Show results
+        document.getElementById('results').style.display =  'block';
+
+        // Hide loader
+        document.getElementById('loading').style.display =  'none';
     } else {
         showEror('Please check your numbers');
     }
-
-    e.preventDefault();
 }
 
 // Show Error
 function showEror(error){
+     // Hide results
+     document.getElementById('results').style.display =  'none';
+
+     // Hide loader
+     document.getElementById('loading').style.display =  'none';
+
     //  Create div
     const errorDiv = document.createElement('div');
 
